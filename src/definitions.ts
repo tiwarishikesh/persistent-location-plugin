74575cdff4faf8afcb137b8b73aa94856cb91005
Rishikesh Tiwari <rishi@kanris.biz>
2023-11-19 15:13:39 +0530

    initial commit

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,10 @@
+declare module "@capacitor/core"{
+  interface PluginRegistry{
+    persistantLocationPlugin : PersistentLocationPlugin
+  }
+}
+
+
 export interface PersistentLocationPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
 }
